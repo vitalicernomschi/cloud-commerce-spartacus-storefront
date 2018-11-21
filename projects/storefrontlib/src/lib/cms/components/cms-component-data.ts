38c330b3e7eabcb63b07bc40647421cac6209434
Tobias Ouwejan <tobias.ouwejan@sap.com>
2018-11-21 11:40:04 +0100

    Make new component type optional

--- projects/storefrontlib/src/lib/cms/components/cms-component-data.ts
+++ projects/storefrontlib/src/lib/cms/components/cms-component-data.ts
@@ -2,7 +2,7 @@ import { Observable } from 'rxjs';
 
 export abstract class CmsComponentData {
   uid: string;
-  type: string;
+  type?: string;
   contextParameters: any;
   data$: Observable<any>;
 }
